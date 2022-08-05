022017c86bc336003f04b2f0ad2e4911a7f4e252
MaxiiXx23 <max.232017@gmail.com>
2022-08-04 22:05:00 -0300

    Iniciando a criação da leitura de arquivos csv com a funções de stream do Node.js

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,15 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
     // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
+        const parserFile = parse();
+        stream.pipe(parserFile);
+        parserFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
