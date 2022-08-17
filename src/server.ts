185abd8dbb20219a09766971a47ea5351038f7d2
MaxiiXx23 <max.232017@gmail.com>
2022-08-16 21:03:36 -0300

    Utilizando e configurando o docker e docker compose para rodar de forma local

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("Server is running with Docker!"));
